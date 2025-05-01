bb598dee262eb42bdb9e0d1509d60ec1638b81e0
Imtisal Sahil Ansari <imtisalsahil@gmail.com>
2025-05-01 22:47:28 +0530

    shl_recommendation_app.py

--- shl_recommendation_app.py
+++ shl_recommendation_app.py
@@ -18,3 +18,26 @@ def health_check():
 def recommend(query: Query):
     top_results = search_assessments(query.query, embeddings, assessments, index)
     return {"recommendations": top_results}
+@app.post("/recommend")
+def recommend(query: Query):
+    query_vec = model.encode([query.query])
+    distances, indices = index.search(query_vec, 10)
+
+    seen = set()
+    results = []
+
+    for idx in indices[0]:
+        a = assessments[idx]
+        unique_key = a["name"] + a["url"]
+        if unique_key not in seen:
+            seen.add(unique_key)
+            results.append({
+                "name": a["name"],
+                "url": a["url"],
+                "remote": a["remote"],
+                "adaptive": a["adaptive"],
+                "duration": a["duration"],
+                "type": a["type"]
+            })
+
+    return {"recommendations": results}
