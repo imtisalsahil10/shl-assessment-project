6b94e4e7da3e6ece600d0b8ca525c32c29d283a7
Imtisal Sahil Ansari <imtisalsahil@gmail.com>
2025-05-01 21:40:19 +0530

    streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,9 @@ import requests
 
 st.title("SHL Assessment Recommender")
 
+API_URL = "https://shl-assessment-project.onrender.com"
+res = requests.post(API_URL, json={"query": query})
+
 query = st.text_area("Enter job description or query:")
 if st.button("Recommend"):
     res = requests.post("http://localhost:8000/recommend", json={"query": query})
